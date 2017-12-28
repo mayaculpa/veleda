d78d3ce0a996bd05614a95684d2ff4528b1b5da3
Moritz Ulmer <moritz.ulmer@posteo.de>
2017-12-28 15:17:22 +0530

    Initial commit

---
+++ flask-thymedata/__init__.py
@@ -0,0 +1,6 @@
+import flask from Flask
+
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_object('config.default')
+app.config.from_pyfile('config.py')
+app.config.from_envvar('APP_CONFIG_FILE')
