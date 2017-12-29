545fa862a5b8029bbd64c570db76ef8ecc7768ad
Moritz Ulmer <moritz.ulmer@posteo.de>
2017-12-29 14:31:40 +0530

    Add failing tests for user registration and authentication

---
+++ web/thymedata/tests/test_basic.py
@@ -0,0 +1,67 @@
+import os
+import unittest
+
+from thymedata import app, db, mail
+
+
+TEST_DB = 'test.db'
+
+
+class BasicTests(unittest.TestCase):
+
+    ############################
+    #### setup and teardown ####
+    ############################
+
+    # executed prior to each test
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+            os.path.join(app.config['BASEDIR'], TEST_DB)
+        self.app = app.test_client()
+        db.drop_all()
+        db.create_all()
+
+        mail.init_app(app)
+        self.assertEqual(app.debug, False)
+
+    # executed after each test
+    def tearDown(self):
+        pass
+
+    ###############
+    #### Tests ####
+    ###############
+
+    def test_main_page(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_valid_user_registration(self):
+        response = self.register('user@example.com',
+                                 'Password$1',
+                                 'Password$1')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Thanks for registering!', response.data)
+
+    def test_invalid_user_registration_different_passwords(self):
+        response = self.register('user@example.com',
+                                 'Password$1',
+                                 'Password$2')
+        self.assertIn(b'Field must be equal to password.', response.data)
+
+    def test_invalid_user_registration_duplicate_email(self):
+        response = self.register('user@example.com',
+                                 'Password$1',
+                                 'Password$1')
+        self.assertEqual(response.status_code, 200)
+        response = self.register('user@example.com',
+                                 'Password$3',
+                                 'Password$3')
+        self.assertIn(b'ERROR! Email (user@example.com) already exists.', response.data)
+
+
+if __name__ == "__main__":
+    unittest.main()
