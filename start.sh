70395ab985d738746c7db44a284a6fb86fadc577
Moritz Ulmer <moritz.ulmer@posteo.de>
2018-02-10 19:31:23 +0100

    Add nginx container for serving static Jekyll site

--- start.sh
+++ start.sh
@@ -4,9 +4,18 @@ normal=$(tput sgr0)
 echo "${bold}veleda: Building Jekyll site${normal}"
 docker run --rm \
   --volume="$PWD/jekyll:/srv/jekyll" \
-  --volume="jekyll-cache:/usr/local/bundle" \
+  --volume="veledaio_jekyll-cache:/usr/local/bundle" \
   -it jekyll/jekyll:latest \
   jekyll build
+
+echo "${bold}veleda: Copying site to jekyll-data volume${normal}"
+docker run \
+  --name helper \
+  --volume="veledaio_jekyll-data:/web" \
+  -it busybox \
+  true
+docker cp jekyll/web/. helper:/web
+docker rm helper
 
 echo "${bold}veleda: Building stack${normal}"
 docker-compose build
