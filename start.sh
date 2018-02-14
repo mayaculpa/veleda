6838598030e3e9d02dbef0024d584e12a0429b9d
Moritz Ulmer <moritz.ulmer@posteo.de>
2018-02-14 23:30:58 +0100

    Fix Docker volume dependency on parent folder issue
    By default the parent folder name (veleda) is appended to the Docker volumes. This was thymedata and then veledaio, but has to be changed to the name of the Git repository, veleda.

--- start.sh
+++ start.sh
@@ -4,14 +4,14 @@ normal=$(tput sgr0)
 echo "${bold}veleda: Building Jekyll site${normal}"
 docker run --rm \
   --volume="$PWD/jekyll:/srv/jekyll" \
-  --volume="veledaio_jekyll-cache:/usr/local/bundle" \
+  --volume="veleda_jekyll-cache:/usr/local/bundle" \
   -it jekyll/jekyll:latest \
   jekyll build
 
 echo "${bold}veleda: Copying site to jekyll-data volume${normal}"
 docker run \
   --name helper \
-  --volume="veledaio_jekyll-data:/web" \
+  --volume="veleda_jekyll-data:/web" \
   -it busybox \
   true
 docker cp jekyll/web/. helper:/web
