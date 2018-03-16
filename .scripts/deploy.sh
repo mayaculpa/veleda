a63862d336a3b9903db0b7ce5558975ba8dd6d3d
Moritz Ulmer <moritz.ulmer@posteo.de>
2018-03-16 13:58:24 +0000

    Change deploy structure to use bare git repo
    The bare git repo is located in the 'repo' folder while the working directory is located in the sibling folder 'veleda'

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -2,14 +2,15 @@
 
 if [ $TRAVIS_BRANCH != 'master' ] ; then
     echo "Pushing to staging"
-    git remote add deploy "deploy@staging.veleda.io:/home/deploy/veleda/"
+    git remote add deploy "deploy@staging.veleda.io:/home/deploy/repo/"
 else
     echo "Pushing to production"
-    git remote add deploy "deploy@veleda.io:/home/deploy/veleda/"
+    git remote add deploy "deploy@veleda.io:/home/deploy/repo/"
 fi
 
 git push -f deploy $TRAVIS_BRANCH
 
-ssh -t deploy@staging.veleda.io "cd veleda \
-                                 && git checkout -f $TRAVIS_BRANCH \
-                                 && ./start.sh"
+ssh -t deploy@staging.veleda.io "mkdir -p veleda &&
+    git --work-tree=../veleda --git-dir=./repo checkout -f &&
+    cd ../veleda &&
+    ./start.sh"
