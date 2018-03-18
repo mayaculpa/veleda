49058a3eb813836899c51e32eb03a30bf44e670f
Moritz Ulmer <moritz.ulmer@posteo.de>
2018-03-18 14:52:45 +0100

    Move DNS variables to separate dns.* files

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -3,6 +3,15 @@
 # exit with nonzero exit code if anything fails
 set -e
 
+# Add the SSH login key
+chmod 600 veleda-deploy-key
+mv veleda-deploy-key ~/.ssh/id_rsa
+
+# Register the Veleda staging and production server SSH keys
+echo '|1|BqdQKtUnA/AtCT/p2M7wgMq3wlY=|lH39cRtAE64wd6EG3ry2J9ewXic= ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBH3antqwy3D4NVVfHQX3SQc/g4wl/SAVC9w9QEry7hhQmB0SJIprwNAq8Hy2DzVCS7kTj/q7fCiiL7oAznrax+0=' >> $HOME/.ssh/known_hosts
+echo '|1|+Z7oOsZ+zdL6u8o8VSWp+bRzd2g=|XMw2HyJIHoekOYlJYw1n75plL2E= ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBCJ/fa/mr577/qCuRXqUNccfmhpUtmi46LSyE7nDbOgxv8kZFs7yQ/sh6TM5npR+ZIbe9I0qmdvA+cE1QfvN21E=' >> $HOME/.ssh/known_hosts
+
+# Select the remote to push to depending on the branch
 if [ $TRAVIS_BRANCH != 'production' ] ; then
     echo "Pushing to staging"
     export REMOTE=staging.veleda.io
@@ -11,9 +20,11 @@ else
     export REMOTE=veleda.io
 fi
 
+# Push to the remote server
 git remote add deploy "deploy@$REMOTE:/home/deploy/repo/"
 git push -f deploy $TRAVIS_BRANCH
 
+# Unpack and update the Docker services
 ssh -t deploy@"$REMOTE" "\
     mkdir -p veleda &&
     git --work-tree=./veleda --git-dir=./repo checkout -f $TRAVIS_BRANCH &&
---
+++ debugging.md
@@ -0,0 +1,5 @@
+# Debugging
+
+## PostgreSQL
+
+Connect to the database with `psql -U web_user -W -h localhost -p 5432 web_db` or for admin access `psql -U postgres -W -h localhost -p 5432`.
