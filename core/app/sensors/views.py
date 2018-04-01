a526203bb280805d6af00e04045a85c0bd213933
Moritz Ulmer <moritz.ulmer@posteo.de>
2018-04-01 16:39:14 +0200

    Add manage database view for sensors endpoint

--- core/app/sensors/views.py
+++ core/app/sensors/views.py
@@ -1,7 +1,9 @@
 from flask import render_template
+from flask_login import login_required
 
 from . import sensors
 
 @sensors.route('/')
+@login_required
 def index():
     return render_template('sensors/index.html')
