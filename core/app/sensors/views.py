b7fb3353f7e35a27873b480985c6520dd33d9cbc
Moritz Ulmer <moritz.ulmer@posteo.de>
2018-04-01 17:32:16 +0200

    Add initial template for database view

--- core/app/sensors/views.py
+++ core/app/sensors/views.py
@@ -1,9 +1,9 @@
 from flask import render_template
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 from . import sensors
 
 @sensors.route('/')
 @login_required
 def index():
-    return render_template('sensors/index.html')
+    return render_template('sensors/index.html', influx_dbs=current_user.influx_dbs)
