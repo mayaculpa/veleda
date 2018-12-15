6900210a4c9309ddb694f7527794b0db1226ff02
Moritz Ulmer <moritz.ulmer@posteo.de>
2018-12-15 11:24:55 +0100

    Remove remaining references to veleda

--- start.sh
+++ start.sh
@@ -1,30 +1,31 @@
 bold=$(tput bold)
 normal=$(tput sgr0)
 
-echo "${bold}veleda: Creating data volumes${normal}"
-docker volume create veleda_influxdb-data
-docker volume create veleda_grafana-data
-docker volume create veleda_postgres-data
-docker volume create veleda_jekyll-data
+# Prefix 'flow-leaf-server' has to match the parent deploy folder on the server
+echo "${bold}flow-leaf-server: Creating data volumes${normal}"
+docker volume create flow-leaf-server_influxdb-data
+docker volume create flow-leaf-server_grafana-data
+docker volume create flow-leaf-server_postgres-data
+docker volume create flow-leaf-server_jekyll-data
 
-echo "${bold}veleda: Building Jekyll site${normal}"
+echo "${bold}flow-leaf-server: Building Jekyll site${normal}"
 docker run --rm \
   --volume="$PWD/jekyll:/srv/jekyll" \
   --volume="jekyll-cache:/usr/local/bundle" \
   -it jekyll/jekyll:latest \
   jekyll build
 
-echo "${bold}veleda: Copying site to jekyll-data volume${normal}"
+echo "${bold}flow-leaf-server: Copying site to jekyll-data volume${normal}"
 docker run \
   --name helper \
-  --volume="veleda_jekyll-data:/web" \
+  --volume="flow-leaf-server_jekyll-data:/web" \
   -it busybox \
   true
 docker cp jekyll/web/. helper:/web
 docker rm helper
 
-echo "${bold}veleda: Building stack${normal}"
+echo "${bold}flow-leaf-server: Building stack${normal}"
 docker-compose build
 
-echo "${bold}veleda: Starting stack${normal}"
+echo "${bold}flow-leaf-server: Starting stack${normal}"
 docker-compose up -d
