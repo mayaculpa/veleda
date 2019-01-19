e11531358d74552682750f8d221ed65c90b8df0d
Moritz Ulmer <moritz.ulmer@posteo.de>
2019-01-19 22:51:36 +0100

    Remove Flask core and replace with Django
    Why:
    
    - Flask community support is smaller
    - Less work to develop with Django
    - Admin interface and better user support in Django
    
    This change addresses the need by:
    
    - Removing the core folder
    - Creating an initial Django project

--- core/app/sensors/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-sensors = Blueprint('sensors', __name__)
-
-from . import views # noqa
--- core/app/sensors/forms.py
+++
@@ -1,6 +0,0 @@
-from flask_wtf import Form
-from wtforms import SubmitField
-
-class EditInfluxDBForm(Form):
-    reset = SubmitField()
-    delete = SubmitField()
