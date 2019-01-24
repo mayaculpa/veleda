cab3e096f5556d79fa5a6238807caac7398944c7
Moritz Ulmer <moritz.ulmer@posteo.de>
2019-01-24 18:41:24 +0100

    Implement OAuth2 authentication code test
    Why:
    
    - Verify with CI that OAuth2 works
    
    This change addresses the need by:
    
    - Add an OAuth2 test that goes from start till end

--- core/core/tests/test_oauth2.py
+++ core/core/tests/test_oauth2.py
@@ -1,5 +1,9 @@
 from rest_framework.test import APIClient
-from rest_framework import status
+import base64
+import urllib
+import json
+from uuid import uuid4
+
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
 from django.urls import reverse
@@ -7,7 +11,11 @@ from django.urls import reverse
 from oauth2_provider.models import Application
 
 
-class Oauth2Test(TestCase):
+class Oauth2ApplicationTests(TestCase):
+    """
+    Tests the OAuth2 functionality
+    """
+
     def auth_redirect_test(self, view):
         """
         Tests that the selected oauth2 view redirects
@@ -20,7 +28,6 @@ class Oauth2Test(TestCase):
 
     def setUp(self):
         self.http_client = Client()
-        self.api_client = APIClient()
 
         self.a_user = User.objects.create_user(
             username="a_user", email="a@example.com", password="12345"
@@ -32,11 +39,11 @@ class Oauth2Test(TestCase):
         self.restricted_views = ["list", "register"]
         self.oauth2_client = Application.objects.create(
             client_id="1234",
+            client_secret="abcd",
             user=self.super_user,
             redirect_uris="localhost:8000",
             client_type="confidential",
-            authorization_grant_type="hello",
-            client_secret="abcd",
+            authorization_grant_type="authorization-code",
             name="test_client",
         )
 
@@ -76,3 +83,94 @@ class Oauth2Test(TestCase):
         )
         self.assertEqual(response.status_code, 404)
 
+
+class Oauth2AuthenticationTests(TestCase):
+    """
+    Tests the OAuth2 authentication
+    """
+
+    def setUp(self):
+        self.a_user = User.objects.create_user(
+            username="a_user", email="a@example.com", password="12345"
+        )
+        self.super_user = User.objects.create_superuser(
+            username="super_user", email="super@example.com", password="12345"
+        )
+
+        self.oauth2_client = Application.objects.create(
+            client_id="1234",
+            client_secret="abcd",
+            user=self.super_user,
+            redirect_uris="http://oauth2_client_ip/callback",
+            client_type="confidential",
+            authorization_grant_type="authorization-code",
+            name="test_client",
+        )
+        self.oauth2_client.save()
+
+    def tearDown(self):
+        pass
+
+    def test_authorization_flow(self):
+        state = str(uuid4())
+        params = {
+            "client_id": self.oauth2_client.client_id,
+            "response_type": "code",
+            "state": state,
+            "redirect_uri": self.oauth2_client.redirect_uris,
+            "duration": 3600,
+            "scope": "userinfo-v1",
+        }
+        authorize_url = (
+            reverse("oauth2_provider:authorize") + "?" + urllib.parse.urlencode(params)
+        )
+
+        # Request the authorization form
+        authorization_client = Client()
+        self.assertTrue(authorization_client.login(username="a_user", password="12345"))
+        response = authorization_client.get(authorize_url, follow=True)
+        self.assertEqual(response.status_code, 200)
+
+        # Submit the authorization form
+        authorization_data = {
+            "csrfmiddlewaretoken": response.context["csrf_token"].__str__(),
+            "allow": "Authorize",
+            "redirect_uri": response.context_data["redirect_uri"],
+            "scope": response.context_data["scopes"][0],
+            "client_id": response.context_data["client_id"],
+            "state": response.context_data["state"],
+            "response_type": response.context_data["response_type"],
+        }
+        response = authorization_client.post(authorize_url, authorization_data)
+        self.assertEqual(response.status_code, 302)
+
+        # Parse the redirect and request an access token
+        token_client = Client()
+
+        auth_string = (
+            self.oauth2_client.client_id + ":" + self.oauth2_client.client_secret
+        )
+        credentials = base64.b64encode(auth_string.encode())
+        token_client.defaults["HTTP_AUTHORIZATION"] = "Basic " + credentials.decode(
+            "ascii"
+        )
+
+        response_data = urllib.parse.parse_qs(urllib.parse.urlparse(response.url).query)
+        token_data = {
+            "grant_type": "authorization_code",
+            "code": response_data["code"][0],
+            "redirect_uri": self.oauth2_client.redirect_uris,
+        }
+
+        response = token_client.post(reverse("oauth2_provider:token"), data=token_data)
+        token_json = json.loads(response.content)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("access_token", token_json)
+
+        # Request user data with obtained token
+        userinfo_client = Client()
+        userinfo_client.defaults["Authorization"] = (
+            "Bearer " + token_json["access_token"]
+        )
+        response = userinfo_client.get(reverse("api-v1-userinfo"))
+        self.assertEqual(response.status_code, 200)
