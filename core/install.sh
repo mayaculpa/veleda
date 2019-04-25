167190a0d9efa98470ee228f78afa70884d445e0
Moritz Ulmer <moritz.ulmer@posteo.de>
2019-04-25 12:08:40 +0200

    Add CI support for updated core
    Why:
    
    - Allow tests and coverage to be run
    
    This change addresses the need by:
    
    - Adding Travis CI integration
    - Adding Codecov integration
    - Adding install and test scripts

---
+++ core/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# exit with nonzero exit code if anything fails
+set -e
+
+# Install Pipenv
+pip install pipenv
+
+# Install Python dependencies
+cd "$(dirname "$0")"
+pipenv sync -d
