f3edccf46c470af18f3f47decdbcf08f73f23f30
Moritz Ulmer <moritz.ulmer@posteo.de>
2019-04-26 08:25:20 +0200

    Create admin user on startup
    Why:
    
    - Allow admin login
    
    This change addresses the need by:
    
    - Adding admin user creation to the startup script

--- core/core/settings.py
+++ core/core/settings.py
@@ -27,9 +27,9 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = os.environ.get("VIRTUAL_HOST", "core.flowleaf.local,localhost").split(
-    ","
-)
+ALLOWED_HOSTS = os.environ.get(
+    "VIRTUAL_HOST", "core.flowleaf.local,localhost,127.0.0.1"
+).split(",")
 
 # Application definition
 
@@ -132,8 +132,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 STATIC_URL = "/static/"
